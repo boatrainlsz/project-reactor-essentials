f66cd0bb79977ad1a8bd2adfde53a63cc2dd0c7f
boatrainlsz <yinger521999@gmail.com>
2022-08-16 22:26:12 +0800

    monoDoOnMethods

--- src/test/java/MonoTest.java
+++ src/test/java/MonoTest.java
@@ -1,6 +1,5 @@
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -12,9 +11,7 @@ public class MonoTest {
         Mono<String> mono = Mono.just(name).log();
         mono.subscribe();
         log.info("------------");
-        StepVerifier.create(mono)
-                .expectNext(name)
-                .verifyComplete();
+        StepVerifier.create(mono).expectNext(name).verifyComplete();
     }
 
     @Test
@@ -23,40 +20,31 @@ public class MonoTest {
         Mono<String> mono = Mono.just(name).log();
         mono.subscribe(s -> log.info("Subscriber: {}", s));
         log.info("------------");
-        StepVerifier.create(mono)
-                .expectNext(name)
-                .verifyComplete();
+        StepVerifier.create(mono).expectNext(name).verifyComplete();
     }
 
     @Test
     public void monoSubscriberConsumerError() {
         String name = "Taylor Swift";
-        Mono<String> mono = Mono.just(name)
-                .map(s -> {
-                    throw new RuntimeException("Something went wrong");
-                });
+        Mono<String> mono = Mono.just(name).map(s -> {
+            throw new RuntimeException("Something went wrong");
+        });
         mono.subscribe(s -> log.info("Subscriber: {}", s), Throwable::printStackTrace);
         log.info("------------");
-        StepVerifier.create(mono)
-                .expectError(RuntimeException.class)
-                .verify();
+        StepVerifier.create(mono).expectError(RuntimeException.class).verify();
     }
 
     @Test
     public void monoSubscriberConsumerComplete() {
         String name = "Taylor Swift";
-        Mono<String> mono = Mono.just(name)
-                .log()
-                .map(String::toUpperCase);
+        Mono<String> mono = Mono.just(name).log().map(String::toUpperCase);
         mono.subscribe(s -> log.info("Subscriber: {}", s), Throwable::printStackTrace, () -> log.info("Completed"));
     }
 
     @Test
     public void monoSubscriberConsumerSubscription() {
         String name = "Taylor Swift";
-        Mono<String> mono = Mono.just(name)
-                .log()
-                .map(String::toUpperCase);
+        Mono<String> mono = Mono.just(name).log().map(String::toUpperCase);
         mono.subscribe(s -> log.info("Subscriber: {}", s), Throwable::printStackTrace, () -> log.info("Completed"),
                 subscription -> subscription.request(11));
     }
@@ -64,13 +52,15 @@ public class MonoTest {
     @Test
     public void monoDoOnMethods() {
         String name = "Taylor Swift";
-        Mono<String> mono = Mono.just(name)
+        Mono<Object> mono = Mono.just(name)
 //                .log()
                 .doOnSubscribe(subscription -> log.info("Subscribed"))
                 .doOnRequest(num -> log.info("Requested: {}", num))
                 .doOnNext(s -> log.info("Next: {}", s))
-                .doOnSuccess(s -> log.info("Success: {}", s))
-                .map(String::toUpperCase);
+                .flatMap(s -> Mono.empty())
+                //这一行不会被执行，因为上一步传来了Mono.empty()
+                .doOnNext(s -> log.info("Next: {}", s))
+                .doOnSuccess(s -> log.info("Success: {}", s));
         mono.subscribe(s -> log.info("Subscriber: {}", s), Throwable::printStackTrace, () -> log.info("Completed"),
                 subscription -> subscription.request(11));
     }
