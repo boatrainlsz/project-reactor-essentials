ed3afeb12a779126e1090825d5830527676238e8
boatrainlsz <yinger521999@gmail.com>
2022-08-18 21:19:11 +0800

    fluxSubscriberError

--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
@@ -8,16 +9,32 @@ public class FluxTest {
     @Test
     public void fluxSubscriber() {
         Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5).log();
-        StepVerifier.create(flux)
-                .expectNext(1, 2, 3, 4, 5)
-                .verifyComplete();
+        StepVerifier.create(flux).expectNext(1, 2, 3, 4, 5).verifyComplete();
     }
 
     @Test
     public void fluxSubscriberRange() {
         Flux<Integer> flux = Flux.range(1, 5).log();
-        StepVerifier.create(flux)
-                .expectNext(1, 2, 3, 4, 5)
-                .verifyComplete();
+        StepVerifier.create(flux).expectNext(1, 2, 3, 4, 5).verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberFromList() {
+        Flux<Integer> flux = Flux.fromIterable(List.of(1, 2, 3, 4, 5)).log();
+        StepVerifier.create(flux).expectNext(1, 2, 3, 4, 5).verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberError() {
+        Flux<Integer> flux = Flux.range(1, 5).log().map(i -> {
+            if (i == 3) {
+                throw new RuntimeException("Error");
+            }
+            return i;
+        });
+        flux.subscribe(i -> log.info("{}", i), Throwable::printStackTrace, () -> log.info("Completed"));
+        StepVerifier.create(flux).expectNext(1, 2)
+                .expectError(RuntimeException.class)
+                .verify();
     }
 }
